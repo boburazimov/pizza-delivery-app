8685aaf4f7aa218f2c4b4b63c40b4fb648d18d83
uzbek <ard_admin@mail.ru>
2020-08-18 19:11:18 +0500

    adding entities

--- app-crm-system-server/src/main/java/uz/crm/system/entity/catalog/Restorant.java
+++ app-crm-system-server/src/main/java/uz/crm/system/entity/catalog/Restorant.java
@@ -2,7 +2,7 @@ package uz.crm.system.entity.catalog;
 
 import uz.crm.system.entity.template.AbsEntity;
 
-public class Restoran extends AbsEntity {
+public class Restorant extends AbsEntity {
 
     private String adress;
 
