ed21bb71ec70fe55cbf757835daa46a351d6fdf2
uzbek <ard_admin@mail.ru>
2020-08-18 13:06:44 +0500

    add server side

---
+++ app-crm-system-server/src/main/java/uz/crm/system/AppCrmSystemApplication.java
@@ -0,0 +1,13 @@
+package uz.crm.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCrmSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppCrmSystemApplication.class, args);
+	}
+
+}
